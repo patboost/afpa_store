efb9509f58b48e47b7ed7fe1a1a1fa99c7c369dc
Patrick <contact@prcdl.fr>
2021-04-23 18:45:18 +0200

    Page produits => correction bug colonne entête + ajout Edition/ Delete dasn storeController

--- src/Controller/StoreController.php
+++ src/Controller/StoreController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -25,5 +27,21 @@ class StoreController extends AbstractController
      */
     public function home() {
         return $this->render('store/home.html.twig');
+    }
+
+    /**
+     * @Route("/store/delete/{id}", name="delete_prod")
+     */
+    public function deleteProduct(Product $prod, EntityManagerInterface $manager) {
+        $manager->remove($prod);
+        $manager->flush();
+        return $this->redirectToRoute('store');
+    }
+
+    /**
+     * @Route ("/store/edit/{id}", name="edit_prod")
+     */
+    public function editProduct(Product $product){
+        return $this->render('store/edit_prod.html.twig');
     }
 }
